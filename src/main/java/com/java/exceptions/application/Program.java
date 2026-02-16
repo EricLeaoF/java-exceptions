0ae1ac60af86e9bc3dfcca534f2c3d9fa7fc10d0
ericleao <ericlf2015@hotmail.com>
2026-02-16 19:38:12 +0000

    reservation class created

---
+++ src/main/java/com/java/exceptions/application/Program.java
@@ -0,0 +1,11 @@
+package com.java.exceptions.application;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/main/java/com/java/exceptions/entities/Reservation.java
@@ -0,0 +1,56 @@
+package com.java.exceptions.entities;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    private final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation() {
+
+    }
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+        Duration diff = Duration.between(getCheckIn(), getCheckOut());
+
+        return diff.toDays();
+    }
+
+    public void updateDates(LocalDate checkIn, LocalDate checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation: Room " + roomNumber + ", check-in: " + dateFormat.format(checkIn) + ", check-out: " + dateFormat.format(checkOut) + ", " + duration() + " nights";
+    }
+}
